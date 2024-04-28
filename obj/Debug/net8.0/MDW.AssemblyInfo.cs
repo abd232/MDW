9458d2903a55f946cead8721fd291724e3140e56
abd232 <abdoss232353@gmail.com>
2024-04-28 20:35:36 +0300

    new

--- obj/Debug/net8.0/MDW.AssemblyInfo.cs
+++ obj/Debug/net8.0/MDW.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MDW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80269a39a533fe72c4c67e25cd850abb24b14175")]
 [assembly: System.Reflection.AssemblyProductAttribute("MDW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MDW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
